389a3f09036a9225c72270344f03b3a416ad9ba4
Dmitry <pochta.nadomne@mail.ru>
2023-07-13 21:59:00 +0300

    4.1. Разбор первой программы (Исправление Checkstyle)  [#159033 #429804]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Дмитрий");
         System.out.println("10.12.2004");
     }
