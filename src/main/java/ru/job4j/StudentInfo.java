13efc461d375caf1280b80cb8cbfee212cb46f2f
Dmitry <pochta.nadomne@mail.ru>
2023-07-13 21:54:57 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Дмитрий");
+        System.out.println("10.12.2004");
+    }
+}
