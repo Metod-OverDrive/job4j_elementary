38c6337dea4ae5669124fc6d106d06fd71204bc6
Dmitry <pochta.nadomne@mail.ru>
2023-07-18 21:36:41 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -9,5 +11,11 @@ public class ArrayDefinition {
         System.out.println("Prices " + prices.length);
         System.out.println("Surnames " + surname.length);
 
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Oleg";
+        names[2] = "Dima";
+        names[3] = "Boris";
+        System.out.println(Arrays.toString(names));
     }
 }
