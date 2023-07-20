e38a1c3cd8b16c090415855e3258a49e1b7dccae
Dmitry <pochta.nadomne@mail.ru>
2023-07-21 00:49:37 +0300

    6.6.0. Двумерный массив [#226796  [#226796]]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 1;
+        array[0][2] = 1;
+        array[1][0] = 1;
+        array[1][1] = 1;
+        array[1][2] = 1;
+        array[2][0] = 1;
+        array[2][1] = 1;
+        array[2][2] = 1;
+
+    }
+}
