dc392e60a799848b342b4ddb84a2e66feb1717cc
Dmitry <pochta.nadomne@mail.ru>
2023-08-18 13:58:52 +0300

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,9 +1,20 @@
 package ru.job4j;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Multiple {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            System.out.println("1 * " + i + " = " + i);
+        try (var out = new FileOutputStream("data/data_res.txt")) {
+
+            for (int i = 1; i < 10; i++) {
+                out.write(("1 * " + i + " = " + i).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
